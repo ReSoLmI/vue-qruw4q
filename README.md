c8b2f3d79faeecd03d534af18a3ec96e0fb28fc7
ReSoLmI <45628935+ReSoLmI@users.noreply.github.com>
2023-09-21 14:28:12 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-qruw4q
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-kd7qw4)
---
+++ src/main.js
@@ -0,0 +1,7 @@
+const { createApp } = require('vue');
+import App from './App.vue';
+import Element from 'element-ui';
+import Vue from 'vue';
+Vue.use(Element);
+
+createApp(App).mount('#app');
